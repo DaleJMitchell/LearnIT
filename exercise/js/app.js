a84e0fe39425c74d696fdfa3ead5f7118dcbe79a
Darya Taraban <daryastaraban@gmail.com>
2023-03-24 13:14:09 -0400

    display problem, next step: event listeners

--- exercise/js/app.js
+++ exercise/js/app.js
@@ -1,11 +1,13 @@
 let questions = [];
-let unansweredQuestionIndex = [];
+//let unansweredQuestionIndex = [];
 let score = 0;
 const numProblems = 10;
 
 document.addEventListener("DOMContentLoaded", () => {
   generateProblems(numProblems);
   displayNextProblem();
+
+
 });
 
 function generateProblem() {
@@ -16,29 +18,54 @@ function generateProblem() {
   let wrongOne = getRandomNumber(100);
   let wrongTwo = getRandomNumber(100);
   let wrongThree = getRandomNumber(100);
+  let answers = [correctAnswer, wrongOne, wrongTwo, wrongThree];
+  shuffleArray(answers);
+
   return {
     numOne: numOne,
     numTwo: numTwo,
-    wrongOne: wrongOne,
-    wrongTwo: wrongTwo,
-    wrongThree: wrongThree,
     correctAnswer: correctAnswer,
+    answers: answers
   };
 }
 
 function generateProblems(num) {
   for (let i = 0; i < num; i++) {
-    questions.push(generateProblem);
-    unansweredQuestionIndex.push(i);
+    questions.push(generateProblem());
+    //unansweredQuestionIndex.push(i);
   }
+  shuffleArray(questions);
 }
 
 function displayNextProblem() {
-  let problemNumber = getRandomNumber(numProblems);
-  let problem = questions[problemNumber];
-  const expression = document.querySelector(".expression");
-  expression.innerText = `${problem.numOne} * ${problem.numTwo}`;
+  //let problemNumber = getRandomNumber(numProblems);
+  if(questions.length > 0){
+    let problem = questions.pop();
+    const expression = document.querySelector(".expression");
+    expression.innerText = `${problem.numOne} * ${problem.numTwo}`;
+  
+    const answersOnpage = Array.from(document.querySelectorAll("li"));
+    answersOnpage.forEach((answerOnPage) => {
+      answerOnPage.innerText = problem.answers.pop();
+
+      // answerOnpage.addEventListener('click', (event) => {
+      //   if(event.target.innerText = problem.correctAnswer){
+      //     score++;
+      //     //displayNextProblem();
+      //   }
+      //   else {
+      //     let showhide = Array.from(document.getElementsByClassName("show-hide"));
+      //     showhide.foreach((element) => {           //stuff
+      //       })
+      //     }
+      //   })
+      })
+    }
+
 }
+  
+
+
 
 /**
  * Utility function to generate a random number based on max
